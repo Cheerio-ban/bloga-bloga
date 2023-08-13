f264f7e732df745966890de2d704f0d4e8fc68d3
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-08-13 23:37:35 +0100

    Created database and Implemented login and logout functionalities

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=run.py
+DATABASE_URL = "sqlite:///app.db"
--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
 
 
 # A script at the top level must hold the app variable
