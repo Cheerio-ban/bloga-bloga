01f14b1d65a5c84d64148cbf4010e5f430cc35b4
Chiemezie Jacob <Chiemezie.jacob@gmail.com>
2023-08-14 00:58:07 +0100

    Implemented register functionality

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +9,23 @@ class LoginForm(FlaskForm):
     password = PasswordField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember_me')
     submit = SubmitField('Sign In')
+
+
+class RegistrationForm(LoginForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username).first()
+        if user:
+            raise ValidationError('Please use a different username')
+        
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email).first()
+        if email:
+            raise ValidationError('Please use a different email address.')
+
